4804cca2bb908fc1cd379d5406b2b02432b6027c
Ram Bharathi <58993044+serkudiki@users.noreply.github.com>
2024-10-08 22:59:37 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-
-    silent: true,
-    org: "javascript-mastery",
-project: "javascript-nextjs",
-
-
-
     output: 'export',
     typescript: {
         ignoreBuildErrors: true,
